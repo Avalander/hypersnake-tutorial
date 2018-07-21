2dbceee8bd3a153c90e78948be0bd7368e648d00
Avalander <darth.quesorico@gmail.com>
2018-07-21 12:31:48 +0200

    Create background.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,35 @@
-document.body.innerHTML = '<h1>Hello world!</h1>'
+import { app } from 'hyperapp'
+import { withFx } from '@hyperapp/fx'
+import { div } from '@hyperapp/html'
+import { g, rect, svg } from './svg';
+
+
+const SIZE = 15
+const WIDTH = SIZE * 40
+const HEIGHT = SIZE * 27
+
+const COLORS = {
+	background: '#088c64',
+}
+
+const state = {
+
+}
+
+const actions = {
+
+}
+
+const view = state =>
+	div([
+		svg({ viewBox: `0 0 ${WIDTH} ${HEIGHT}`, width: WIDTH, height: HEIGHT}, [
+			Background(),
+		]),
+	])
+
+const Background = () =>
+	g({ key: 'background' }, [
+		rect({ x: 0, y: 0, width: WIDTH, height: HEIGHT, fill: COLORS.background }),
+	])
+
+const game = withFx(app) (state, actions, view, document.body)
---
+++ src/svg.js
@@ -0,0 +1,6 @@
+import { h } from 'hyperapp'
+
+
+export const svg = (attrs, children) => h('svg', attrs, children)
+export const g = (attrs, children) => h('g', attrs, children)
+export const rect = (attrs, children) => h('rect', attrs, children)
