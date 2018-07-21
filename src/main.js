6e2d734e157d16f4abc8f04d9ce08be7d998f332
Avalander <darth.quesorico@gmail.com>
2018-07-21 12:41:54 +0200

    Add snake.

--- src/main.js
+++ src/main.js
@@ -10,10 +10,18 @@ const HEIGHT = SIZE * 27
 
 const COLORS = {
 	background: '#088c64',
+	snake: {
+		fill: '#bcaba0',
+		stroke: '#706660',
+	},
 }
 
 const state = {
-
+	snake: [
+		{ x: 3 * SIZE, y: 3 * SIZE},
+		{ x: 2 * SIZE, y: 3 * SIZE},
+		{ x: 1 * SIZE, y: 3 * SIZE},
+	]
 }
 
 const actions = {
@@ -24,6 +32,7 @@ const view = state =>
 	div([
 		svg({ viewBox: `0 0 ${WIDTH} ${HEIGHT}`, width: WIDTH, height: HEIGHT}, [
 			Background(),
+			Snake(state.snake),
 		]),
 	])
 
@@ -31,5 +40,15 @@ const Background = () =>
 	g({ key: 'background' }, [
 		rect({ x: 0, y: 0, width: WIDTH, height: HEIGHT, fill: COLORS.background }),
 	])
+
+const Snake = state =>
+	g({ key: 'snake' },
+		state.map(({ x, y }) => rect({
+			x, y, width: SIZE, height: SIZE,
+			fill: COLORS.snake.fill,
+			stroke: COLORS.snake.stroke,
+			'stroke-width': 2
+		}))
+	)
 
 const game = withFx(app) (state, actions, view, document.body)
